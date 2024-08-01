842c7fa1dc26609d7779aff3dc1d5f4221693080
Ivan-ee <smirnowwwivan@mail.ru>
2024-08-01 13:25:58 +0300

    .

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -17,47 +17,49 @@ export const Footer = () => {
                         <div>Посмотреть боксы</div>
                     </div>
                 </div>
-                <div className="body">
-                    <div className="desc regular_h3">
-                        <div>+7 (992) 402-52-32</div>
-                        <div>faq@getbox.com</div>
-                    </div>
-                    <div className="text">
-                        <div className={'item'}>
-                            <div className={'top regular_h3'}>
-                                Адрес
+                <div className={'pc'}>
+                    <div className="body">
+                        <div className="desc regular_h3">
+                            <div>+7 (992) 402-52-32</div>
+                            <div>faq@getbox.com</div>
+                        </div>
+                        <div className="text">
+                            <div className={'item'}>
+                                <div className={'top regular_h3'}>
+                                    Адрес
+                                </div>
+                                <div className={'bottom regular_h4'}>
+                                    Краснодар, улица Ягодина, 50/1
+                                </div>
                             </div>
-                            <div className={'bottom regular_h4'}>
-                                Краснодар, улица Ягодина, 50/1
+                            <div className={'item'}>
+                                <div className={'top regular_h3'}>
+                                    График работы
+                                    склада
+                                </div>
+                                <div className={'bottom regular_h4'}>
+                                    Круглосуточно
+                                </div>
+                            </div>
+                            <div className={'item'}>
+                                <div className={'top regular_h3'}>
+                                    График работы ресепшена
+                                </div>
+                                <div className={'bottom regular_h4'}>
+                                    Ежедневно с 9:00 до 21:00
+                                </div>
                             </div>
                         </div>
-                        <div className={'item'}>
-                            <div className={'top regular_h3'}>
-                                График работы
-                                склада
-                            </div>
-                            <div className={'bottom regular_h4'}>
-                                Круглосуточно
-                            </div>
-                        </div>
-                        <div className={'item'}>
-                            <div className={'top regular_h3'}>
-                                График работы ресепшена
-                            </div>
-                            <div className={'bottom regular_h4'}>
-                                Ежедневно с 9:00 до 21:00
-                            </div>
-                        </div>
                     </div>
-                </div>
-                <div className="reference">
-                    <div className={'text regular_h5'}>
-                        <div className={'item'}>Правила аренды</div>
-                        <div className={'item'}>Договор хранения</div>
-                        <div className={'item'}>Политика конфиденциальности</div>
-                        <div className={'item'}>Пользовательское соглашение</div>
+                    <div className="reference">
+                        <div className={'text regular_h5'}>
+                            <div className={'item'}>Правила аренды</div>
+                            <div className={'item'}>Договор хранения</div>
+                            <div className={'item'}>Политика конфиденциальности</div>
+                            <div className={'item'}>Пользовательское соглашение</div>
+                        </div>
+                        <div className={'copyright'}>© GetBox, {currentYear}</div>
                     </div>
-                    <div className={'copyright'}>© GetBox, {currentYear}</div>
                 </div>
             </div>
         </div>
